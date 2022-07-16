aabb0b0a6e421ece8aad637fe904269f056c0b91
nir zamir <nirozamir@gmail.com>
2022-07-17 01:27:46 +0300

    class library

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PlayerBase;
+using EntityBase;
+using SpikeBase;
+using WallBase;
 using PacMan;
-
-Board board = new Board(new IEntity[] { new Player(),new Wall(10,10),new Spike(5,5),new E()});
+Board board = new Board(new IEntity[] { new Player(),new Wall(10,10),new Spike(5,5)});
